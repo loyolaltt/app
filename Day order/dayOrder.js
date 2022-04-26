3ac40a4e3990d1fbe70a7f00e786057fe6e7a9a0
loyolaltt <104448409+loyolaltt@users.noreply.github.com>
2022-04-26 21:06:36 +0530

    LTT app

---
+++ Day order/dayOrder.js
@@ -0,0 +1,74 @@
+
+
+
+document.getElementById("dayimage").src="./dayorder.png"
+// console.log("hello")
+
+
+
+
+// var staticUrl="https://script.google.com/macros/s/AKfycbxX2SfOe0TGfgt27EsPULqhDUHQqYTr5rs6YW2YBnjCBUU4ha6X85aT0CJm0JBB3Wrf/exec"
+
+// var x;
+// var y;
+// var arrayContains=false;
+// var asterisk="(*) No day order given for specific date";
+
+// function dayorder(){
+    
+//     var data=$.getJSON(staticUrl, function(data){
+    
+//         // console.log("Hello");
+
+// x=document.getElementById("udate").value;
+// console.log(x);
+// // y=document.getElementById("day_o").value;
+// // console.log(y)
+
+
+//  // Fetching the needed Value from JSON API
+//  var results=[];
+//  var searchField = "Datee";   //Datee is the column name in spreadsheet
+//  var searchVal=x;
+
+//  for(var i=0;i<data.user.length;i++){
+//      if(data.user[i][searchField]==searchVal){
+//          results.push(data.user[i].Day);
+//      }
+//  }
+
+// arrayContains=results.indexOf("*")>-1;
+// console.log(arrayContains);  
+
+// // checks for results array contains any value
+// if(results.length==0){
+//     document.getElementById("no_val").innerHTML="Invalid Date Format";
+//         console.log("Wrong Date Format");
+//         document.getElementById("undef").style.display="none";
+// // location.reload();
+// // reload
+// setTimeout(function (){
+//     location.reload(1);
+// },5000);
+  
+// }
+// // when the result is (*) the boolean value change and dispaly the message accordingly
+// if(arrayContains === true){
+//     console.log("It is *");
+//     document.getElementById("undef").innerHTML=asterisk;
+
+// }
+
+
+// else{
+
+//  document.getElementById("undef").innerHTML=results[0];
+
+// }
+// }
+//     );
+   
+   
+// }
+
+
---
+++ day.js
@@ -0,0 +1,43 @@
+
+var staticUrl="https://script.google.com/macros/s/AKfycbwuUBr8SVCJS6jXYNV0NzxzjjjTW7EGhmDS-Uxboc23a4Q3J7jwlEJa3e7QJyf6ef2O/exec"
+
+var data=$.getJSON(staticUrl, function(data){
+
+    console.log("Hello");
+
+    var today=new Date();
+    var dd=String(today.getDate()).padStart(2,'0');  //january is 0
+    var mm=String(today.getMonth()+1).padStart(2,'0');
+    var yyyy=today.getFullYear();
+
+    today = mm+'/'+dd+'/'+yyyy;
+    // console.log(today); 
+
+
+
+    // Fetching the needed Value from JSON API
+    var results=[];
+    var searchField = "Datee";
+    var searchVal=today;
+
+    for(var i=0;i<data.user.length;i++){
+        if(data.user[i][searchField]==searchVal){
+            results.push(data.user[i].Day);
+        }
+    }
+    // console.log(results[0]);
+
+    document.getElementById("do").innerHTML=results[0];
+
+
+
+
+}
+);
+
+if(navigator.onLine){
+    alert("Online");
+}
+else{
+    alert("Offline");
+}
